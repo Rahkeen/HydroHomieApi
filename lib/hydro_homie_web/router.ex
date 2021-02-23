4ce8086fe4b67e83a663f1ce91b4c2a15321fbae
rmarfatia <rmarfatia@pinterest.com>
2021-02-22 20:29:26 -0800

    create days endpoint

--- lib/hydro_homie_web/router.ex
+++ lib/hydro_homie_web/router.ex
@@ -13,16 +13,12 @@ defmodule HydroHomieWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", HydroHomieWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
-  end
-
   # Other scopes may use custom stacks.
-  # scope "/api", HydroHomieWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HydroHomieWeb do
+    pipe_through :api
+
+    resources "/days", DayController
+  end
 
   # Enables LiveDashboard only for development
   #
